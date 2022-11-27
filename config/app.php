d506942528fa61a7ea3242373a3e84471c5b782e
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-11-26 20:01:06 -0400

    si

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+        
+
     ],
 
     /*
@@ -229,6 +233,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
 
     ],
 
