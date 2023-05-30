f883ad56c71384be44eebf03d8e0720be0009470
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-05-30 13:38:45 -0400

    si

--- resources/views/layouts/template-print-alt.blade.php
+++ resources/views/layouts/template-print-alt.blade.php
@@ -55,10 +55,10 @@
     @yield('css')
 </head>
 <body>
-    <div class="hide-print" style="text-align: right; padding: 10px 0px">
+    {{-- <div class="hide-print" style="text-align: right; padding: 10px 0px">
         <button class="btn-print" onclick="window.close()">Cancelar <i class="fa fa-close"></i></button>
         <button class="btn-print" onclick="window.print()"> Imprimir <i class="fa fa-print"></i></button>
-    </div>
+    </div> --}}
     <div id="watermark">
         <img src="{{ asset('images/icon.png') }}" /> 
     </div>
